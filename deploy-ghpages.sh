af5cdab48b8d977c6780778be78a293733689e6e
John <johnjmartiniv@gmail.com>
2015-02-07 12:46:09 -0500

    merh

--- deploy-ghpages.sh
+++ deploy-ghpages.sh
@@ -6,8 +6,7 @@ node build.js
  git init
  git config user.name "Travis-CI"
  git config user.email "travis@nodemeatspace.com"
- cp ../CNAME ./CNAME
- cp ../countryiso.js ./countryiso.js
+ cp -r ./app/ ./
  git add .
  git commit -m "Deployed to Github Pages"
  git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
